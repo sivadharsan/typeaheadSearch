cc798f81d7100eaa93d177ddcc6033a52e957de0
sellathurai_s <sivadharsan.sellathurai@getinsured.com>
2017-09-22 09:53:19 -0700

    first commit

---
+++ typehead.js
@@ -0,0 +1,45 @@
+var typeHead = (function(){
+    "use strict";
+    var searchResultsDropDown = document.getElementById('searchResultsDropDown');
+    function getData(){
+        var searchTextValue = $("#searchText").val();
+        this.searchResults = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+          ];
+
+        var that = this;
+        setTimeout(function()
+          {
+               var html = '<select>';
+               var startValue = $("#searchText").val()[0];
+               for(var i = 0; i < that.searchResults.length; i++) {
+                    if(that.searchResults[i].startsWith(startValue))
+                      html += "<option value='" + that.searchResults[i]+ "'>" + that.searchResults[i] + "</option>";
+               }
+               html += '</select>';
+               searchResultsDropDown.innerHTML= html;
+
+               searchResultsDropDown.style.visibility = "visible";
+          }, 500);
+    };
+
+    function hideDropDown(){
+      searchResultsDropDown.style.visibility = "hidden";
+    }
+
+    return {
+      getData: getData,
+      hideDropDown: hideDropDown
+    }
+}());
